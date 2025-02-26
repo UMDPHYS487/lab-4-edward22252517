4ad5f448eb22ac4127bd79591f8029494b517733
Wei-Ju Lin <93338333+edward22252517@users.noreply.github.com>
2025-02-26 18:56:00 +0000

    Ex2

---
+++ loop2-ex2.cpp
@@ -0,0 +1,14 @@
+// add your code here
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int n = 10;
+    
+    while (n>0) {
+        cout << "loop count is " << n << endl;
+        --n;
+    }
+
+}
