2dbdac7c2fa8f07a53a50ccb52b2e0eda60dc156
Wei-Ju Lin <93338333+edward22252517@users.noreply.github.com>
2025-02-26 21:00:38 +0000

    ex7

--- SwitchCase-ex7.cpp
+++ SwitchCase-ex7.cpp
@@ -1,1 +1,33 @@
-// add your code here
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    int n = 0;  // Initialize n to avoid undefined behavior
+    cin >> i;
+    if (i % 2 == 0 && i < 10) {
+        n = 1;
+    }
+    else if (i % 2 == 1) {
+        n = 2;
+    }
+    else {
+        n = 3;
+    }    
+    switch (n)
+    {
+        case 1:
+            cout << i * i << endl;
+            break;  // Prevent fall-through
+            
+        case 2:              
+            cout << i / 2 << endl;
+            break;
+            
+        case 3:              
+            cout << "haha" << endl;
+            break;
+    }
+    return 0;
+}
