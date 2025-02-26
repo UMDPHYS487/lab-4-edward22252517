fe5dc4932474374b18d67c64d146a1add7a8b0e5
Wei-Ju Lin <93338333+edward22252517@users.noreply.github.com>
2025-02-26 19:12:29 +0000

    ex3

--- Factorial-ex3.cpp
+++ Factorial-ex3.cpp
@@ -1,1 +1,19 @@
 // Add your code here
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int n = 10;
+    int i = 0;
+    int fac = 1;
+    while (n>0) {
+        i += 1;
+        fac *= i;
+        
+
+        cout << "factorial of " << i << "is" << fac << endl;
+        --n;
+    }
+
+}
